3cc2e31cf1ccdd58d46ab75b6d19d626cda62142
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2017-07-20 23:09:13 +0100

    added limit option to polls controller

--- TH.Elections.PollsApi/Controllers/PollsController.cs
+++ TH.Elections.PollsApi/Controllers/PollsController.cs
@@ -22,10 +22,25 @@ namespace TH.Elections.PollsApi.Controllers
 
         // GET: api/Polls
         [HttpGet]
-        public IEnumerable<Polls> GetPolls()
+        public IEnumerable<Polls> GetPolls(int? limit = null)
         {
-            var x = _context.Polls.ToList().Select(p => { p.PartyVotingIntentions = null; return p; });
-            return x;
+            if (limit == null)
+            {
+                return _context.Polls.ToList().Select(p => { p.PartyVotingIntentions = null; return p; });
+            }
+            else
+            {
+                List<Polls> polls = _context.Polls.OrderByDescending(p => p.PollDate).Take((int)limit).Include(p => p.PartyVotingIntentions).Include(p => p.PollingCompany).ToList();
+                List<int> pollingCompanyIDs = polls.Select(p => p.PollingCompanyId).ToList();
+                List<PollingCompanies> pollingCompanies = _context.PollingCompanies.Where(pc => pollingCompanyIDs.Any(pcid => pcid == pc.PollingCompanyId)).ToList();
+                foreach (Polls poll in polls)
+                {
+                    PollingCompanies pollingCompany = pollingCompanies.Single(pc => pc.PollingCompanyId == poll.PollingCompanyId);
+                    pollingCompany.Polls = null;
+                    poll.PollingCompany = pollingCompany;
+                }
+                return polls;
+            }
         }
 
         // GET: api/Polls/5
